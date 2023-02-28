c1fdc6635d71abe666063e4852011cee9f1c16c9
Ramdane Martin <martin.ramdane@epitech.eu>
2023-02-28 18:19:13 +0100

    add some rules

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,7 @@
 import System.Environment
 import System.Exit
 import Data.Maybe
+import System.Console.Terminal.Size
 
 data Conf = Conf {
   rule :: Maybe Int,
@@ -20,7 +21,7 @@ data Conf = Conf {
 defaultConf :: Conf
 defaultConf = Conf {
   rule = Nothing,
-  start = 0,
+  start = 1,
   nb_lines = Nothing,
   window = 80,
   move = 0
@@ -28,11 +29,16 @@ defaultConf = Conf {
 
 getOpts :: Conf -> [String] -> Maybe Conf
 getOpts conf [] = Just conf
-getOpts conf ("--rule":xs) = getOpts (conf {rule = Just (read (head xs))}) (tail xs)
-getOpts conf ("--start":xs) = getOpts (conf {start = read (head xs)}) (tail xs)
-getOpts conf ("--lines":xs) = getOpts (conf {nb_lines = Just (read (head xs))}) (tail xs)
-getOpts conf ("--window":xs) = getOpts (conf {window = read (head xs)}) (tail xs)
-getOpts conf ("--move":xs) = getOpts (conf {move = read (head xs)}) (tail xs)
+getOpts conf ("--rule":xs) =
+  getOpts (conf {rule = Just (read (head xs))}) (tail xs)
+getOpts conf ("--start":xs) =
+  getOpts (conf {start = read (head xs)}) (tail xs)
+getOpts conf ("--lines":xs) =
+  getOpts (conf {nb_lines = Just (read (head xs))}) (tail xs)
+getOpts conf ("--window":xs) =
+  getOpts (conf {window = read (head xs)}) (tail xs)
+getOpts conf ("--move":xs) =
+  getOpts (conf {move = read (head xs)}) (tail xs)
 getOpts _ _ = Nothing
 
 showConf :: Maybe Conf -> IO ()
@@ -41,16 +47,61 @@ showConf Nothing = putStrLn "Error"
 
 checkErrors :: Maybe Conf -> IO ()
 checkErrors Nothing = exitWith (ExitFailure 84)
-checkErrors (Just conf) = do
+checkErrors (Just conf) =
   if (isNothing (rule conf))
     then exitWith (ExitFailure 84)
   else
     return ()
+
+intToBin :: Int -> [Int]
+intToBin 0 = [0]
+intToBin 1 = [1]
+intToBin n = intToBin (n `div` 2) ++ [n `mod` 2]
+
+binToEightsBytes :: [Int] -> [Int]
+binToEightsBytes [] = []
+binToEightsBytes (x:xs) = if (length xs < 7)
+  then (replicate (7 - length xs) 0) ++ (x:xs)
+  else x:(binToEightsBytes xs)
+
+getNextLine :: [Int] -> String -> Char -> String
+getNextLine _ [_] _ = " "
+getNextLine _ [_,_] _ = " "
+getNextLine _ "" _ = " "
+getNextLine bin ('*':'*':'*':xs) c =
+  c : getNextLine bin ('*':'*':xs) (if (bin !! 0) == 1 then '*' else ' ')
+getNextLine bin ('*':'*':' ':xs) c =
+  c : getNextLine bin ('*':' ':xs) (if (bin !! 1) == 1 then '*' else ' ')
+getNextLine bin ('*':' ':'*':xs) c =
+  c : getNextLine bin (' ':'*':xs) (if (bin !! 2) == 1 then '*' else ' ')
+getNextLine bin ('*':' ':' ':xs) c =
+  c : getNextLine bin (' ':' ':xs) (if (bin !! 3) == 1 then '*' else ' ')
+getNextLine bin (' ':'*':'*':xs) c =
+  c : getNextLine bin ('*':'*':xs) (if (bin !! 4) == 1 then '*' else ' ')
+getNextLine bin (' ':'*':' ':xs) c =
+  c : getNextLine bin ('*':' ':xs) (if (bin !! 5) == 1 then '*' else ' ')
+getNextLine bin (' ':' ':'*':xs) c =
+  c : getNextLine bin (' ':'*':xs) (if (bin !! 6) == 1 then '*' else ' ')
+getNextLine bin (' ':' ':' ':xs) c =
+  c : getNextLine bin (' ':' ':xs) (if (bin !! 7) == 1 then '*' else ' ')
+getNextLine bin (_:y:z:xs) c = ' ' : getNextLine bin (y:z:xs) c
+
+wolfram :: Maybe Conf -> String -> [Int] -> Int -> IO ()
+wolfram _ _ [] _ = return ()
+wolfram arg li bin n
+  | n > fromJust (nb_lines (fromJust arg)) = exitWith(ExitSuccess)
+  | otherwise = putStrLn li >> wolfram arg (getNextLine bin li ' ') bin (n + 1)
 
 main :: IO ()
 main = do
   args <- getArgs
   let conf = getOpts defaultConf args
   checkErrors conf
-  showConf conf
+  let bin = binToEightsBytes (intToBin (fromJust (rule (fromJust conf))))
+  size <- size
+  let (Window height width) = fromMaybe (Window 80 24) size
+  let first = replicate ((width `div` 2) + (move (fromJust conf))) ' ' ++ "*"
+  let scd = replicate ((width `div` 2) - 1 - (move (fromJust conf))) ' '
+  let prevLine = first ++ scd
+  wolfram conf prevLine bin 0
   return ()
